14314942b9320e9dda2dd7b5ed5e94cce654e3b2
Catalyst <danielduyilemi@gmail.com>
2024-02-01 00:08:11 +0100

    Catalyst first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,9 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+
+        // Custom Colors
+        backgroundColor: 'F5F7F8', 
       },
       borderRadius: {
         lg: "var(--radius)",
