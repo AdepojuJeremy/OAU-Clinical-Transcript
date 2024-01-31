ade687ccb8d8117ff62bc62e706890694afb1c7d
unknown <oyebolaebun07@gmail.com>
2024-01-31 12:57:04 +0100

    shadcn and next js setup

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+// import useScreenSize from "./components/useScreenSize";
+
+// import { Provider } from "react-redux";
+// import store from "@/store/store";
+export default function Home() {
+  // const { isDesktop, isMobile, isTablet } = useScreenSize();
+  return (
+    // <Provider store={store}>
+    //   <div className="md:flex md:justify-center">
+    //     {isMobile && <NavMobile />}
+
+    <div>
+      <h1 className="font-crimson">app is running</h1>
+    </div>
+    // </Provider>
+  );
+}
