aef339dbb89417b7188480b30ea47029b90ed32b
unknown <oyebolaebun07@gmail.com>
2024-01-31 23:58:23 +0100

    shadcn ui componenets

---
+++ components/Form.jsx
@@ -0,0 +1,23 @@
+"use client"
+
+export default function Form(){
+    return(
+        <Form>
+  <FormField
+    control={...}
+    name="..."
+    render={() => (
+      <FormItem>
+        <FormLabel />
+        <FormControl>
+          { /* Your form field */}
+        </FormControl>
+        <FormDescription />
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+</Form>
+
+    )
+}
