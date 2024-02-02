30cbf63c68005bf77a9da816af35a395f0e51fd6
unknown <oyebolaebun07@gmail.com>
2024-02-02 04:57:48 +0100

    started work today and installed some components

--- app/(routes)/login/page.jsx
+++ app/(routes)/login/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderOne from "@/components/HeaderOne";
+import LoginForm from "@/components/LoginForm";
 
 function page() {
   return (
     <div>
       <HeaderOne />
+      <LoginForm />
     </div>
   );
 }
