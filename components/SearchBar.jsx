c208588ad0da4fc8fe7a2cc8e8f9216483d9dd68
Catalyst <danielduyilemi@gmail.com>
2024-02-02 05:12:48 +0100

    Add searchbar features.

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useRef, useState, useEffect } from "react";
+import Link from "next/link";
 import {
   Command,
   CommandDialog,
@@ -10,12 +12,99 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function SearchBar() {
+  const suggestions = useRef(null);
+  const search = useRef(null);
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  const handleClickOutside = (e) => {
+    if (search && !search.current.contains(e.target)) {
+      setSearchOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <Command className='-mt-[8%] w-[50%] rounded-full flex-row justify-between items-center '>
-      <CommandInput className='italic text-primaryBlue placeholder:text-primaryBlue' placeholder="Search by Name or Matric No." />
-      <img src="./mi_filter.png" alt="mi_filter" className="h-8 w-8 mr-10"/>
+    <Command
+      ref={search}
+      className={`${
+        !searchOpen ? "-mt-[15%]" : ""
+      } w-[40%] rounded-xl justify-between items-center px-4`}
+    >
+      <div
+        className={`flex flex-row w-full justify-between items-center gap-6 ${
+          searchOpen ? "border-b" : ""
+        } `}
+      >
+        <CommandInput
+          onClick={() => setSearchOpen(true)}
+          className="italic text-primaryBlue placeholder:text-primaryBlue border-none"
+          placeholder="Search by Name or Matric No."
+        />
+        <img src="./mi_filter.svg" alt="mi_filter" className="h-8 w-8 " />
+      </div>
+
+      {searchOpen && (
+        <>
+          <div className="filter w-full flex justify-between items-center text-primaryBlue py-4">
+            <div>Filter by</div>
+            <div className="filter-tags flex-1 flex justify-between max-w-[65%]">
+              
+              <div className="graduation filter-tag">Graduation</div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <div className="admission filter-tag">Admission Year</div>
+              <div className="criterion filter-tag">Criterion X</div>
+            </div>
+            <div className="filter-icon">
+              <img src="./typcn_filter.svg" alt="typcn_filter" />
+            </div>
+          </div>
+          <CommandList ref={suggestions} className="w-full">
+            <CommandGroup>
+              {Array.from({ length: 9 }, (_, index) => 1 + index).map(
+                (num, i) => (
+                  <CommandItem key={i} className="flex justify-between">
+                    <div className="flex gap-4">
+                      <span>CLI/2013/00{num}</span> <span>Aboma Ciroma A.</span>
+                    </div>{" "}
+                    <div>
+                      <Link className="text-oauOrange" href={""}>
+                        View Results
+                      </Link>
+                    </div>
+                  </CommandItem>
+                )
+              )}
+            </CommandGroup>
+          </CommandList>
+        </>
+      )}
     </Command>
   );
 }
