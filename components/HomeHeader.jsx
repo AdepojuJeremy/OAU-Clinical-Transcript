2e44c499525eac4c7f68c5004432d4d9409aa982
Catalyst <danielduyilemi@gmail.com>
2024-02-03 07:15:33 +0100

    Some search bar features.

--- components/HomeHeader.jsx
+++ components/HomeHeader.jsx
@@ -11,17 +11,17 @@ import {
 
 function HomeHeader() {
   return (
-    <nav className="absolute inset-x-0 flex justify-center items-center py-10 text-[30px] z-[100]">
-      <div className="w-[90%] grid grid-flow-col items-center">
+    <nav className="absolute inset-x-0 flex justify-center items-center py-10 text-xl 2xl:text-[28px] z-[100]">
+      <div className="w-[98%] grid grid-flow-col items-center">
         <div className="logo flex-1">
-          <img src="./oaulogo.png" alt="oaulogo" />
+          <img src="./oaulogo.png" className="w-[100px] 2xl:w-[116px]" alt="oaulogo" />
         </div>
-        <div className="absolute text-[40px] uppercase max-w-[30vw] text-center font-bold text-primaryBlue justify-self-center left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%]">
-          OAU TRANSCRIPT RETRIEVAL PORTAL
+        <div className="absolute flex flex-col items-center text-[32px]/[3rem] 2xl:text-[40px] text uppercase w-[40vw] text-center font-bold text-primaryBlue justify-self-center left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%]">
+          <span>OAU TRANSCRIPT</span> 
+          <span>RETRIEVAL PORTAL</span>
         </div>
         <Menubar className='justify-self-end'>
           <MenubarMenu>
-
             <MenubarTrigger>Home</MenubarTrigger>
             <MenubarTrigger className='text-oauOrange'>Search</MenubarTrigger>
             <MenubarTrigger>Results</MenubarTrigger>
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef, useState, useEffect } from "react";
 import Link from "next/link";
+import students from "@/schem.json";
 import {
   Command,
   CommandDialog,
@@ -25,6 +26,32 @@ function SearchBar() {
   const suggestions = useRef(null);
   const search = useRef(null);
   const [searchOpen, setSearchOpen] = useState(false);
+  const [admissionDropOpen, setAdmissionDropOpen] = useState(false);
+  const [graduationDropOpen, setGraduationDropOpen] = useState(false);
+  const [filterOpen, setFilterOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredStudents, setFilteredStudents] = useState(students);
+
+  const filteredSearch = (e) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    // Filter the students based on the matric number or name
+    const filteredList = students.filter(
+      (student) =>
+        student.matricNo.toLowerCase().includes(value.toLowerCase()) ||
+        student.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setFilteredStudents(filteredList);
+  };
+
+  useEffect(() => {
+    if (admissionDropOpen) setGraduationDropOpen(false);
+  }, [admissionDropOpen]);
+  useEffect(() => {
+    if (graduationDropOpen) setAdmissionDropOpen(false);
+  }, [graduationDropOpen]);
 
   const handleClickOutside = (e) => {
     if (search && !search.current.contains(e.target)) {
@@ -44,7 +71,7 @@ function SearchBar() {
     <Command
       ref={search}
       className={`${
-        !searchOpen ? "-mt-[15%]" : ""
+        !searchOpen ? "" : "mt-[10%]"
       } w-[40%] rounded-xl justify-between items-center px-4`}
     >
       <div
@@ -57,50 +84,95 @@ function SearchBar() {
           className="placeholder:italic text-primaryBlue placeholder:text-primaryBlue border-none"
           placeholder="Search by Name or Matric No."
         />
-        <img src="./mi_filter.svg" alt="mi_filter" className="h-8 w-8 " />
+        <img
+          src="./mi_filter.svg"
+          alt="mi_filter"
+          className="h-8 w-8 "
+          onClick={() => setFilterOpen(!filterOpen)}
+        />
       </div>
 
       {searchOpen && (
         <>
-          <div className="filter w-full flex justify-between items-center text-primaryBlue py-4">
-            <div>Filter by</div>
-            <div className="filter-tags flex-1 flex justify-between max-w-[65%]">
-              
-              <div className="graduation filter-tag">Graduation</div>
-              <DropdownMenu>
-                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Profile</DropdownMenuItem>
-                  <DropdownMenuItem>Billing</DropdownMenuItem>
-                  <DropdownMenuItem>Team</DropdownMenuItem>
-                  <DropdownMenuItem>Subscription</DropdownMenuItem> */}
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <div className="admission filter-tag">Admission Year</div>
-              <div className="criterion filter-tag">Criterion X</div>
+          {filterOpen ? (
+            <div className="filter w-full flex justify-between items-center text-primaryBlue py-4">
+              <div>Filter by</div>
+              <div className="filter-tags flex-1 flex justify-between max-w-[65%]">
+                <div className="graduation relative">
+                  <button
+                    className={`text-white px-2 py-1 rounded-lg text-[16px] ${
+                      graduationDropOpen ? "bg-primaryBlue" : "bg-primaryGray"
+                    }`}
+                    onClick={() => setGraduationDropOpen(!graduationDropOpen)}
+                  >
+                    Graduation Year
+                  </button>
+                  {graduationDropOpen ? (
+                    <div className="admission-dropdown absolute top-[110%] left-[50%] -translate-x-[50%] w-[120%] text-center z-10 opacity-100 visible bg-white shadow-md p-2 rounded-2xl">
+                      <ul className="text-primaryBlue admission-dropdown-list max-h-[20rem] overflow-y-auto">
+                        <li className="font-bold">Graduation Year</li>
+                        <li>119X/9X</li>
+                        <li>119X/9X</li>
+                        <li>119X/9X</li>
+                        <li>2000/0X</li>
+                        <li>2000/0X</li>
+                        <li>2000/0X</li>
+                        <li>2000/0X</li>
+                      </ul>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <div className="admission relative">
+                  <button
+                    className={`text-white px-2 py-1 rounded-lg text-[16px] ${
+                      admissionDropOpen ? "bg-primaryBlue" : "bg-primaryGray"
+                    }`}
+                    onClick={() => setAdmissionDropOpen(!admissionDropOpen)}
+                  >
+                    Admission Year
+                  </button>
+                  {admissionDropOpen ? (
+                    <div className="admission-dropdown absolute top-[110%] left-[50%] -translate-x-[50%] w-[120%] text-center z-10 opacity-100 visible bg-white shadow-md p-2 rounded-2xl">
+                      <ul className="text-primaryBlue admission-dropdown-list max-h-[20rem] overflow-y-auto">
+                        <li className="font-bold">Admission Year</li>
+                        <li>119X/9X</li>
+                        <li>119X/9X</li>
+                        <li>119X/9X</li>
+                        <li>2000/0X</li>
+                        <li>2000/0X</li>
+                        <li>2000/0X</li>
+                        <li>2000/0X</li>
+                      </ul>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <div className="criterion filter-tag">Criterion X</div>
+              </div>
+              <div className="filter-icon">
+                <img src="./typcn_filter.svg" alt="typcn_filter" />
+              </div>
             </div>
-            <div className="filter-icon">
-              <img src="./typcn_filter.svg" alt="typcn_filter" />
-            </div>
-          </div>
+          ) : (
+            <></>
+          )}
           <CommandList ref={suggestions} className="w-full">
             <CommandGroup>
-              {Array.from({ length: 9 }, (_, index) => 1 + index).map(
-                (num, i) => (
-                  <CommandItem key={i} className="flex justify-between">
-                    <div className="flex gap-4">
-                      <span>CLI/2013/00{num}</span> <span>Aboma Ciroma A.</span>
-                    </div>{" "}
-                    <div>
-                      <Link className="text-oauOrange" href={""}>
-                        View Results
-                      </Link>
-                    </div>
-                  </CommandItem>
-                )
-              )}
+              {filteredStudents.map((item, i) => (
+                <CommandItem key={i} className="flex justify-between">
+                  <div className="flex gap-4">
+                    <span></span> <span>Aboma Ciroma A.</span>
+                  </div>{" "}
+                  <div>
+                    <Link className="text-oauOrange" href={""}>
+                      View Results
+                    </Link>
+                  </div>
+                </CommandItem>
+              ))}
             </CommandGroup>
           </CommandList>
         </>
